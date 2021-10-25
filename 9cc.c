b84f3292cb46682bd577132a4dd7f3083eec9899
sueka <mail@sueka.me>
2021-10-25 21:47:33 +0900

    :sparkles: Have the compiler give you where an error occurs

--- 9cc.c
+++ 9cc.c
@@ -35,6 +35,25 @@ void error(char *fmt, ...) {
   exit(1);
 }
 
+// 入力プログラム
+char *user_input;
+
+// エラー箇所を報告する。
+void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, " "); // pos 個の空白を出力
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+
+  exit(2);
+}
+
 // 現在のトークンが期待している記号の場合はトークンを1つ読み進めて真を返す。それ以外の場合は偽を返す。
 bool consume(char op) {
   if (token->kind == TK_RESERVED && token->str[0] == op) {
@@ -49,7 +68,7 @@ bool consume(char op) {
 // 現在のトークンが期待している記号の場合はトークンを1つ読み進める。それ以外の場合はエラーを報告する。
 void expect(char op) {
   if (token->kind != TK_RESERVED || token->str[0] != op) {
-    error("'%c' ではありません", op);
+    error_at(token->str, "'%c' ではありません", op);
   }
 
   token = token->next;
@@ -58,7 +77,7 @@ void expect(char op) {
 // 現在のトークンが数値の場合はトークンを1つ読み進めてその数値を返す。それ以外の場合はエラーを報告する。
 int expect_number() {
   if (token->kind != TK_NUM) {
-    error("数ではありません。");
+    error_at(token->str, "数ではありません。");
   }
 
   int val = token->val;
@@ -108,7 +127,7 @@ Token *tokenize(char *p) {
       continue;
     }
 
-    error("トークナイズできません。");
+    error_at(token->str, "トークナイズできません。");
   }
 
   new_token(TK_EOF, cur, p);
@@ -118,10 +137,12 @@ Token *tokenize(char *p) {
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    error("引数の個数が正しくありません。");
+    error_at(token->str, "引数の個数が正しくありません。");
 
     return 1;
   }
+
+  user_input = argv[1];
 
   // トークナイズする
   token = tokenize(argv[1]);
