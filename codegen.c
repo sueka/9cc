5cc5b92da405b1d91827f8a7e1a35674cb864a4f
sueka <mail@sueka.me>
2021-10-28 16:46:20 +0900

    :sparkles: Generate assignments

--- codegen.c
+++ codegen.c
@@ -1,9 +1,38 @@
 #include "9cc.h"
 
-void gen(Node *node) {
-  if (node->kind == ND_NUM) {
-    printf("  push %d\n", node->val);
+void gen_lval(Node *node) {
+  if (node->kind != ND_LVAR) {
+    error_at(token->str, "代入の左辺値が変数ではありません。");
+  }
 
+  // node のアドレス（rbp から offset を引いた値）を push する
+  printf("  mov rax, rbp\n");
+  printf("  sub rax, %d\n", node->offset);
+  printf("  push rax\n");
+}
+
+void gen(Node *node) {
+  switch (node->kind) {
+    case ND_NUM:
+    printf("  push %d\n", node->val);
+    return;
+    case ND_LVAR:
+    gen_lval(node);
+
+    // スタックトップに node のアドレスがあるので、そこにある値で置き換える。
+    printf("  pop rax\n");
+    printf("  mov rax, [rax]\n");
+    printf("  push rax\n");
+    return;
+    case ND_ASSIGN:
+    gen_lval(node->lhs);
+    gen(node->rhs);
+
+    // スタックトップに代入する値、次に左辺のアドレスがあるので、左辺のアドレスにある値をスタックトップの値で置き換える。代入値をスタックに戻す。
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+    printf("  mov [rax], rdi\n");
+    printf("  push rdi\n");
     return;
   }
 
