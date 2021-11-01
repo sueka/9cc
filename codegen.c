e1368fffeb61944b832a02681af4b220f1863168
sueka <mail@sueka.me>
2021-11-02 03:56:23 +0900

    :art: Indent the case clauses

--- codegen.c
+++ codegen.c
@@ -19,99 +19,99 @@ char *regs[6] = {"rdi", "rsi", "rdx", "rcx", "r8", "r9"};
 void gen(Node *node) {
   switch (node->kind) {
     case ND_NUM:
-    printf("  push %d\n", node->val);
-    return;
+      printf("  push %d\n", node->val);
+      return;
     case ND_DEF:
-    // noop; プロローグで変数26個分の領域を固定で確保しているため、ここでは何もしない。
-    return;
+      // noop; プロローグで変数26個分の領域を固定で確保しているため、ここでは何もしない。
+      return;
     case ND_LVAR:
-    gen_lval(node);
+      gen_lval(node);
 
-    // スタックトップに node のアドレスがあるので、そこにある値で置き換える。
-    printf("  pop rax\n");
-    printf("  mov rax, [rax]\n");
-    printf("  push rax\n");
-    return;
-    case ND_ASSIGN:
-    gen_lval(node->lhs);
-    gen(node->rhs);
-
-    // スタックトップに代入する値、次に左辺のアドレスがあるので、左辺のアドレスにある値をスタックトップの値で置き換える。代入値をスタックに戻す。
-    printf("  pop rdi\n");
-    printf("  pop rax\n");
-    printf("  mov [rax], rdi\n");
-    printf("  push rdi\n");
-    return;
-    case ND_ADDR:
-    gen_lval(node->lhs);
-    return;
-    case ND_DEREF:
-    gen(node->lhs);
-
-    // スタックトップにアドレスがあるので、そこにある値で置き換える。
-    printf("  pop rax\n");
-    printf("  mov rax, [rax]\n");
-    printf("  push rax\n");
-    return;
-    case ND_RETURN:
-    gen(node->lhs);
-
-    printf("  pop rax\n");
-    printf("  mov rsp, rbp\n");
-    printf("  pop rbp\n");
-    printf("  ret\n");
-    return;
-    case ND_IF:
-    gen(node->cond); // スタックトップに結果が入っているはず
-
-    printf("  pop rax\n");
-    printf("  cmp rax, 0\n");
-
-    if (!node->otherwise) { // if
-      printf("  je  .Lend%d\n", c);
-      gen(node->then);
-    } else { // if-else
-      printf("  je  .Lelse%d\n", c);
-      gen(node->then);
-      printf("  jmp .Lend%d\n", c);
-      printf(".Lelse%d:\n", c);
-      gen(node->otherwise);
-    }
-
-    printf(".Lend%d:\n", c);
-
-    ++c;
-    return;
-    case ND_WHILE:
-    printf(".Lbegin%d:\n", c);
-    gen(node->cond);
-    printf("  pop rax\n");
-    printf("  cmp rax, 0\n");
-    printf("  je  .Lend%d\n", c);
-    gen(node->body);
-    printf("  jmp .Lbegin%d\n", c);
-    printf(".Lend%d:\n", c);
-    ++c;
-    return;
-    case ND_FOR:
-    gen(node->init);
-    printf(".Lbegin%d:\n", c);
-    gen(node->cond);
-    printf("  pop rax\n");
-    printf("  cmp rax, 0\n");
-    printf("  je  .Lend%d\n", c);
-    gen(node->body);
-    gen(node->iter);
-    printf("  jmp .Lbegin%d\n", c);
-    printf(".Lend%d:\n", c);
-    ++c;
-    return;
-    case ND_BLOCK:
-    for (int i = 0; node->stmts[i]; ++i) {
-      gen(node->stmts[i]);
+      // スタックトップに node のアドレスがあるので、そこにある値で置き換える。
       printf("  pop rax\n");
-    }
-    return;
+      printf("  mov rax, [rax]\n");
+      printf("  push rax\n");
+      return;
+    case ND_ASSIGN:
+      gen_lval(node->lhs);
+      gen(node->rhs);
+
+      // スタックトップに代入する値、次に左辺のアドレスがあるので、左辺のアドレスにある値をスタックトップの値で置き換える。代入値をスタックに戻す。
+      printf("  pop rdi\n");
+      printf("  pop rax\n");
+      printf("  mov [rax], rdi\n");
+      printf("  push rdi\n");
+      return;
+    case ND_ADDR:
+      gen_lval(node->lhs);
+      return;
+    case ND_DEREF:
+      gen(node->lhs);
+
+      // スタックトップにアドレスがあるので、そこにある値で置き換える。
+      printf("  pop rax\n");
+      printf("  mov rax, [rax]\n");
+      printf("  push rax\n");
+      return;
+    case ND_RETURN:
+      gen(node->lhs);
+
+      printf("  pop rax\n");
+      printf("  mov rsp, rbp\n");
+      printf("  pop rbp\n");
+      printf("  ret\n");
+      return;
+    case ND_IF:
+      gen(node->cond); // スタックトップに結果が入っているはず
+
+      printf("  pop rax\n");
+      printf("  cmp rax, 0\n");
+
+      if (!node->otherwise) { // if
+        printf("  je  .Lend%d\n", c);
+        gen(node->then);
+      } else { // if-else
+        printf("  je  .Lelse%d\n", c);
+        gen(node->then);
+        printf("  jmp .Lend%d\n", c);
+        printf(".Lelse%d:\n", c);
+        gen(node->otherwise);
+      }
+
+      printf(".Lend%d:\n", c);
+
+      ++c;
+      return;
+    case ND_WHILE:
+      printf(".Lbegin%d:\n", c);
+      gen(node->cond);
+      printf("  pop rax\n");
+      printf("  cmp rax, 0\n");
+      printf("  je  .Lend%d\n", c);
+      gen(node->body);
+      printf("  jmp .Lbegin%d\n", c);
+      printf(".Lend%d:\n", c);
+      ++c;
+      return;
+    case ND_FOR:
+      gen(node->init);
+      printf(".Lbegin%d:\n", c);
+      gen(node->cond);
+      printf("  pop rax\n");
+      printf("  cmp rax, 0\n");
+      printf("  je  .Lend%d\n", c);
+      gen(node->body);
+      gen(node->iter);
+      printf("  jmp .Lbegin%d\n", c);
+      printf(".Lend%d:\n", c);
+      ++c;
+      return;
+    case ND_BLOCK:
+      for (int i = 0; node->stmts[i]; ++i) {
+        gen(node->stmts[i]);
+        printf("  pop rax\n");
+      }
+      return;
     case ND_FCALL: {
       int i = 0;
 
@@ -134,38 +134,38 @@ void gen(Node *node) {
       return;
     }
     case ND_FDEFN:
-    printf("%.*s:\n", node->len, node->name);
+      printf("%.*s:\n", node->len, node->name);
 
-    // プロローグ
-    // 変数26個分の領域を確保する
-    printf("  push rbp\n");
-    printf("  mov rbp, rsp\n");
-    printf("  sub rsp, 208\n");
+      // プロローグ
+      // 変数26個分の領域を確保する
+      printf("  push rbp\n");
+      printf("  mov rbp, rsp\n");
+      printf("  sub rsp, 208\n");
 
-    int i;
+      int i;
 
-    for (i = 0; node->args[i]; ++i) {
-      gen_lval(node->args[i]);
-    }
+      for (i = 0; node->args[i]; ++i) {
+        gen_lval(node->args[i]);
+      }
 
-    // RDI, RSI, RDX, RCX, R8, R9 に実引数、スタックトップに上（成長元）から順に仮引数のアドレスがあるので、仮引数のアドレスを pop し、仮引数のアドレスにある値を実引数で置き換える。
-    for (int j = i - 1; j >= 0; --j) {
+      // RDI, RSI, RDX, RCX, R8, R9 に実引数、スタックトップに上（成長元）から順に仮引数のアドレスがあるので、仮引数のアドレスを pop し、仮引数のアドレスにある値を実引数で置き換える。
+      for (int j = i - 1; j >= 0; --j) {
+        printf("  pop rax\n");
+        printf("  mov [rax], %s\n", regs[j]);
+      }
+
+      gen(node->body);
+
+      // 式の評価結果としてスタックに1つの値が残っているはずなので、スタックが溢れないように pop しておく
       printf("  pop rax\n");
-      printf("  mov [rax], %s\n", regs[j]);
-    }
 
-    gen(node->body);
+      // エピローグ
+      // 最後の式の結果が RAX に残っているので、それを返り値とする
+      printf("  mov rsp, rbp\n");
+      printf("  pop rbp\n");
+      printf("  ret\n");
 
-    // 式の評価結果としてスタックに1つの値が残っているはずなので、スタックが溢れないように pop しておく
-    printf("  pop rax\n");
-
-    // エピローグ
-    // 最後の式の結果が RAX に残っているので、それを返り値とする
-    printf("  mov rsp, rbp\n");
-    printf("  pop rbp\n");
-    printf("  ret\n");
-
-    return;
+      return;
   }
 
   gen(node->lhs);
@@ -176,38 +176,38 @@ void gen(Node *node) {
 
   switch (node->kind) {
     case ND_EQ:
-    printf("  cmp rax, rdi\n");
-    printf("  sete al\n");
-    printf("  movzb rax, al\n");
-    break;
+      printf("  cmp rax, rdi\n");
+      printf("  sete al\n");
+      printf("  movzb rax, al\n");
+      break;
     case ND_NE:
-    printf("  cmp rax, rdi\n");
-    printf("  setne al\n");
-    printf("  movzb rax, al\n");
-    break;
+      printf("  cmp rax, rdi\n");
+      printf("  setne al\n");
+      printf("  movzb rax, al\n");
+      break;
     case ND_LT:
-    printf("  cmp rax, rdi\n");
-    printf("  setl al\n");
-    printf("  movzb rax, al\n");
-    break;
+      printf("  cmp rax, rdi\n");
+      printf("  setl al\n");
+      printf("  movzb rax, al\n");
+      break;
     case ND_LTE:
-    printf("  cmp rax, rdi\n");
-    printf("  setle al\n");
-    printf("  movzb rax, al\n");
-    break;
+      printf("  cmp rax, rdi\n");
+      printf("  setle al\n");
+      printf("  movzb rax, al\n");
+      break;
     case ND_ADD:
-    printf("  add rax, rdi\n");
-    break;
+      printf("  add rax, rdi\n");
+      break;
     case ND_SUB:
-    printf("  sub rax, rdi\n");
-    break;
+      printf("  sub rax, rdi\n");
+      break;
     case ND_MUL:
-    printf("  imul rax, rdi\n");
-    break;
+      printf("  imul rax, rdi\n");
+      break;
     case ND_DIV:
-    printf("  cqo\n");
-    printf("  idiv rdi\n");
-    break;
+      printf("  cqo\n");
+      printf("  idiv rdi\n");
+      break;
   }
 
   printf("  push rax\n");
