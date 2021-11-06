0bd05490e74da04f232abd67117978fb66bdf7e7
sueka <mail@sueka.me>
2021-11-06 14:24:34 +0900

    :negative_squared_cross_mark: Add failing expectations for the step 20

--- test.sh
+++ test.sh
@@ -62,5 +62,12 @@ assert 3 'int main() { int x; int *y; y = &x; *y = 3; return x; }'
 assert 4 'int main() { int *p; alloc4(&p, 1, 2, 4, 8); int *q; q = p + 2; return *q; }'
 assert 8 'int main() { int *p; alloc4(&p, 1, 2, 4, 8); int *q; q = p + 2; *q; q = p + 3; return *q; }'
 assert 2 'int main() { int *p; alloc4(&p, 1, 2, 4, 8); int *q; q = p + 3; *q; q = q - 2; return *q; }'
+assert 4 'int main() { int x; return sizeof(x); }'
+assert 8 'int main() { int *y; return sizeof(y); }'
+assert 4 'int main() { int x; return sizeof(x + 3); }'
+assert 8 'int main() { int *y; return sizeof(y + 3); }'
+assert 4 'int main() { int *y; return sizeof(*y); }'
+assert 4 'int main() { return sizeof(1); }'
+assert 4 'int main() { return sizeof(sizeof(1)); }'
 
 echo OK
